7686529852784d39ae13d9c5f0dc1840070ff79c
xynuk <kacperxiamo@gmail.com>
2024-07-12 16:24:38 +0200

    Create btools.lua

---
+++ btools.lua
@@ -0,0 +1,16 @@
+backpackrzrzszsz = game:GetService("Players").LocalPlayer.Backpack
+
+hammer = Instance.new("HopperBin")
+hammer.Name = "Hammer"
+hammer.BinType = 4
+hammer.Parent = backpackrzrzszsz
+
+cloneTool = Instance.new("HopperBin")
+cloneTool.Name = "Clone"
+cloneTool.BinType = 3
+cloneTool.Parent = backpackrzrzszsz
+
+grabTool = Instance.new("HopperBin")
+grabTool.Name = "Grab"
+grabTool.BinType = 2
+grabTool.Parent = backpackrzrzszsz
